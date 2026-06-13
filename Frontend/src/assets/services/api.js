9de35746fe4f438fb075c858e901597678f3f042
Mayank <mayanksankla12140@gmail.com>
2026-06-13 21:53:29 +0530

    use env variable for API URL

--- Frontend/src/assets/services/api.js
+++ Frontend/src/assets/services/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+
 const API = axios.create({
-  baseURL: "http://192.168.1.12:5000/api/student",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
 export default API;
